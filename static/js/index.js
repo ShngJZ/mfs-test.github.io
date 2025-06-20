5630ff29d1cbc2f9baa065947c0ca6be8d1ad0c3
Shengjie Zhu <128062217+ShngJZ@users.noreply.github.com>
2025-06-20 14:48:17 -0700

    fix video playing

--- static/js/index.js
+++ static/js/index.js
@@ -5,16 +5,36 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
+    // Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
+
+    // For each carousel, add event listener to pause/play videos when sliding
+    carousels.forEach(carousel => {
+        carousel.on('before:show', () => {
+            const videos = carousel.element.getElementsByTagName('video');
+            Array.from(videos).forEach(video => {
+                video.pause();
+            });
+        });
+        
+        carousel.on('shown', () => {
+            const currentSlide = carousel.element.querySelector('.slider-item.is-active');
+            if (currentSlide) {
+                const video = currentSlide.querySelector('video');
+                if (video) {
+                    video.play();
+                }
+            }
+        });
+    });
 	
     bulmaSlider.attach();
 
